c1cefb2e339c82de825cfda13f58a51faf6a4807
y.yoshiki <labiatejoker@gmail.com>
2017-07-14 20:00:31 +0900

    Create fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(nickname: "ymd",
+             name:  "yamada",
+             email: "yamada@sample.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             avatar: Faker::Avatar.image("sample", "50x50", "jpg"))
+
+99.times do |n|
+  name  = Faker::Name.name
+  nickname  = Faker::StarWars.character
+  email = "example-#{n+1}@sample.com"
+  password = "password"
+  avatar = Faker::Avatar.image("sample", "50x50", "jpg")
+  User.create!(nickname: nickname,
+               name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               avatar: avatar)
+end
+
+users = User.order(:created_at).take(6)
+
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.tweets.create!(content: content) }
+end
