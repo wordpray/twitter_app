53d97640c40dabb2ff56f0e45e60ee3fc8a4230e
y.yoshiki <labiatejoker@gmail.com>
2017-07-14 20:01:15 +0900

    Add action tweets#create,tweets#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'tweets#home'
   resources :users, only: [:index, :show]
+  resources :tweets, only: [:create, :destroy]
   get '/about', to:'tweets#about'
 end
