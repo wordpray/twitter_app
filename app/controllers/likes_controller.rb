a8a834f517c4a8bf5025d067bb1aa628f0dd54b5
y.yoshiki <labiatejoker@gmail.com>
2017-07-18 20:57:30 +0900

    Correct controller with Likes button implementation

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
   def create
+    @tweet = Tweet.find(params[:tweet_id])
     @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
     @likes = Like.where(tweet_id: params[:tweet_id])
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
+    @tweet = Tweet.find(params[:tweet_id])
     @likes = Like.where(tweet_id: params[:tweet_id])
-    like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
-    if like.destroy
+    @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    if @like.destroy
+      respond_to do |format|
+        format.js
+      end
     else
       redirect_to :back
     end
