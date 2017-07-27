389475fe814d9bbdf8145b1f3f9c33084269ba3f
y.yoshiki <labiatejoker@gmail.com>
2017-07-27 17:53:10 +0900

    Create jquery of character count

--- app/assets/javascripts/tweet_button.js
+++ app/assets/javascripts/tweet_button.js
@@ -1,12 +1,27 @@
-$(function(){
+$(document).on('turbolinks:load', function(){
+  $(function(){
+  $('.tweet_form textarea').on('keyup keydown', function(){
+    var thisValueLength = $(this).val().length;
+    var LeftoverValueLength = 140 - thisValueLength;
 
-$('#tweet-button').on('click', function(){
-  console.log("クリックされたよ！");
+    if (LeftoverValueLength > 10){
+      $('.count').html(LeftoverValueLength);
+    }else{
+      $('.count').html(LeftoverValueLength).css('color', 'red');
+    }
+  });
+
+  $('.reply_form textarea').on('keyup keydown', function(){
+    var thisValueLength = $(this).val().length;
+    var LeftoverValueLength = 140 - thisValueLength;
+    
+    if (LeftoverValueLength > 10){
+      $('.count').html(LeftoverValueLength);
+    }else{
+      $('.count').html(LeftoverValueLength).css('color', 'red');
+    }
+  });
 });
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
 
 });
 
