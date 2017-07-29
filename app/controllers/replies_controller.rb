78f56433bee728e8cf46b3405e79ac76d704d847
y.yoshiki <labiatejoker@gmail.com>
2017-07-29 14:03:01 +0900

    Refactoring replies_controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,22 +1,21 @@
 class RepliesController < ApplicationController
-  before_action :set_users, only:[:new, :create]
+  before_action :set_recommend_users, only:[:new, :create]
   
   def new
-    @tweet = Tweet.find(params[:tweet_id])
-
-    @before_reply = Tweet.find(@tweet.reply_tweet) if @tweet.reply_tweet.present?
+    @tweet         = Tweet.find(params[:tweet_id])
+    @before_reply  = Tweet.find(@tweet.reply_tweet) if @tweet.reply_tweet.present?
     @after_replies = Tweet.where(reply_tweet: @tweet.id)
-    @reply = current_user.tweets.new(reply_nickname: @tweet.user.nickname, reply_tweet: @tweet.id)
-    @likes = Like.where(tweet_id: params[:tweet_id])
+    @reply         = current_user.tweets.new(reply_nickname: @tweet.user.nickname, reply_tweet: @tweet.id)
+    @likes         = Like.where(tweet_id: params[:tweet_id])
   end
 
   def create
     @tweet = current_user.tweets.new(tweet_params)
 
     if @tweet.save
-      flash[:success] = "Reply success"
+      flash[:success] = "返信が送信されました。"
     else
-      flash[:danger] = "Reply danger"
+      flash[:danger] = "返信に失敗しました。"
     end
     redirect_to action: "new"
   end
@@ -27,7 +26,7 @@ class RepliesController < ApplicationController
     params.require(:tweet).permit(:reply_nickname, :reply_tweet, :content, :image)
   end
 
-  def set_users
+  def set_recommend_users
     followings = current_user.following
     following_ids = followings.pluck(:id)
     ids = following_ids.push(current_user.id)
