81b7428ffc8f6a919762dfb0ea954de76123e781
y.yoshiki <labiatejoker@gmail.com>
2017-07-31 15:55:33 +0900

    Fine modification of talk_api.js

--- app/assets/javascripts/talk_api.js
+++ app/assets/javascripts/talk_api.js
@@ -1,33 +1,35 @@
-$(function(){
-  function buildHtml(data){
-    var html = '<li>'+
-                  '<p>'+
-                    data.results[0].reply +
-                  '</p>'
-                '</li>'
-    return html;
-  }
+$(document).on('turbolinks:load', function(){
+  $(function(){
+    function buildHtml(data){
+      var html = '<li>'+
+                    '<p>'+
+                      data.results[0].reply +
+                    '</p>'
+                  '</li>'
+      return html;
+    }
 
-  $('#talkapi__request').on('click', function(){
-    var comment = $('#talkapi__input').val();
+    $('#talkapi__request').on('click', function(){
+      var comment = $('#talkapi__input').val();
 
-    $.ajax({
-      type: "POST",
-      url:"https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk",
-      data:{
-        "apikey":"bzuXlmRoBDncSrVkVedCBe4vT0S4TJHt",
-        "query":comment
-      },
-    })
-    .done(function(data){
-      var api_reply = data.results[0].reply;
-      var html = buildHtml(data);
-      $('#talkapi__reply').text(api_reply);
-      $('#talkapi__input').val("");
-      $('.talk_history').prepend(html);
-    })
-    .fail(function(){
-      alert("メッセージを入れて下さい");
-    })
+      $.ajax({
+        type: "POST",
+        url:"https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk",
+        data:{
+          "apikey":"bzuXlmRoBDncSrVkVedCBe4vT0S4TJHt",
+          "query":comment
+        },
+      })
+      .done(function(data){
+        var api_reply = data.results[0].reply;
+        var html = buildHtml(data);
+        $('#talkapi__reply').text(api_reply);
+        $('#talkapi__input').val("");
+        $('.talk_history').prepend(html);
+      })
+      .fail(function(){
+        alert("メッセージを入れて下さい");
+      })
+    });
   });
 });
